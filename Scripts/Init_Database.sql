d4d6e285b6fd8088696705d14a2fa29574eb060e
Mohamed Taha Abo Hieba <mt1641936@gmail.com>
2025-06-16 04:25:41 +0300

    Create Init_Database.sql

---
+++ Scripts/Init_Database.sql
@@ -0,0 +1,16 @@
+/*
+This script creates a new database named 'DataWarehouse',
+the script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+*/
+
+
+--create the "DataWarehouse" database
+create database DataWarehouse;
+
+use DataWarehouse
+--create schemas
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
